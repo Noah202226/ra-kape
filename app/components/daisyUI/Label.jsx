11a3252da29700a54fced32897353d4196914e65
Noa - DPWH <dpwh.noa@gmail.com>
2025-07-09 17:26:30 +0800

    update 1

---
+++ app/components/daisyUI/Label.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Label() {
+  return (
+    <label className="input">
+      <span className="label">https://</span>
+      <input type="text" placeholder="URL" />
+    </label>
+  );
+}
+
+export default Label;
