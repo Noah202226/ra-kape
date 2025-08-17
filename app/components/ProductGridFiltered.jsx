e2366ac22f299adaf17ab324cfcae308f7f5fd77
Noa <noaligpitan26@gmail.com>
2025-08-17 17:37:26 +0800

    update font size and text-center of category

--- app/components/ProductGridFiltered.jsx
+++ app/components/ProductGridFiltered.jsx
@@ -65,7 +65,7 @@ export default function ProductGridFiltered({ type }) {
 
   return (
     <div className="p-0 md:p-4">
-      <h2 className="text-4xl md:text-8xl font-bold mb-6 capitalize text-right text-black">
+      <h2 className="text-xl md:text-8xl lg:text-9xl font-bold mb-0.5 md:mb-6 capitalize text-center text-black">
         {type.replace(/-/g, " ")}
       </h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 justify-items-center">
