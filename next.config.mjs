cc3ec1603da658d158869c5549a0a23a06376b2e
Noa <dpwh.noa@gmail.com>
2025-10-02 13:05:46 +0800

    upgrade nextjs to latest to fix issue

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   images: {
     domains: ["fra.cloud.appwrite.io", "localhost"],
   },
+  experimental: {
+    optimizeCss: false, // disable Lightning CSS
+  },
 };
 
 export default nextConfig;
